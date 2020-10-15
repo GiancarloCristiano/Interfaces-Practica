d6f4db1fd74ff2eda86ef36868af397c06e2be19
Giancarlo <gian-k@.live.com>
2020-10-15 01:50:36 -0300

    corregido el submit

--- TP3/js/submit.js
+++ TP3/js/submit.js
@@ -1,10 +1,18 @@
 let enviar = document.querySelector('.btn-enviar');
-enviar.addEventListener("click", llenarBarra);
+//let form = document.querySelector('#formContacto');
+enviar.addEventListener('submit', llenarBarra);
+//form.addEventListener('submit', llenarBarra);
+
 
 function llenarBarra(){
+  //form.preventDefault();
   enviar.style.backgroundPosition = '300px 0';
   setTimeout(() => {
-	enviar.innerHTML = '¡Enviado!';
-	enviar.style.animation = "tamanio-boton 0.3s linear";	
-}, 1200);
+    enviar.innerHTML = '¡Enviado!';
+    enviar.style.animation = "tamanio-boton 0.3s linear";	
+  }, 1200);
+  setTimeout(() => {	
+    window.location.reload();
+  }, 2500);
+
 }

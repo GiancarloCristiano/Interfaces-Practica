92e60411e280884ed447d7136109549a4648b8bf
Giancarlo <gian-k@.live.com>
2020-10-19 04:14:17 -0300

    flatarían los comentarios

--- TP3/js/main.js
+++ TP3/js/main.js
@@ -12,8 +12,10 @@ function menuHamburguesa() {
 
 window.onload = setTimeout(function(){
 	document.querySelector('.loader-container').style.display="none";
-	document.querySelector('#parallax').style.display = "block";
-	document.querySelector('#parallax').style.animation = "slide 1s cubic-bezier(0.34, 1.56, 0.64, 1)";
+	if (document.querySelector('#parallax')){
+		document.querySelector('#parallax').style.display = "block";
+		document.querySelector('#parallax').style.animation = "slide 1s cubic-bezier(0.34, 1.56, 0.64, 1)";
+	}
 	document.querySelector('.page').style.display="block";
 	
 }, 3000);

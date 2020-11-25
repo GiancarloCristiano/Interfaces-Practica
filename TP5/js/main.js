c5184d2bb168f5c2831d38c48ebc36ffe521a7b6
Giancarlo <gian-k@.live.com>
2020-11-25 16:21:11 -0300

    range personalizado beta

--- TP5/js/main.js
+++ TP5/js/main.js
@@ -23,3 +23,8 @@ function switchTheme(e) {
 }
 
 //toggleSwitch.addEventListener('change', switchTheme, true);
+
+
+/*document.querySelector(".progress-bar").oninput = function() {
+    this.style.background = 'linear-gradient(to right, #82CFD0 0%, #82CFD0 ' + this.value + '%, #fff ' + this.value + '%, white 100%)'
+  };*/
